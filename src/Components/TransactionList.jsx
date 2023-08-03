a2778bc1b54cf077d8455274ae5056ad4c839ec9
Shourya Pal <palshourya897@gmail.com>
2023-08-03 12:47:51 +0530

    Update TransactionList.jsx

--- src/Components/TransactionList.jsx
+++ src/Components/TransactionList.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
-import Transaction from "./transaction";
+import Transaction from "./Transaction";
 
 const TransactionList = () => {
   const { transactions } = useContext(GlobalContext);
