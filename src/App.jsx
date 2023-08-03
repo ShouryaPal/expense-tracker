93a37e355b51906c5c9567664ae9ce6874b7c34a
Shourya  Pal <104789024+ShouryaPal@users.noreply.github.com>
2023-08-03 12:32:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import AddTransaction from "./Components/AddTransaction";
 import Balance from "./Components/Balance";
 import Header from "./Components/Header";
 import TransactionList from "./Components/TransactionList";
-import IncomeExpenses from "./Components/incomeExpenses";
+import IncomeExpenses from "./Components/IncomeExpenses";
 import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
